3986a64dff5bc76650ff03e8c005c5a4f2a94c17
Amir-UL <mangoleo51@gmail.com>
2023-02-13 18:31:37 +0600

    checkout

--- src/main/kotlin/com/kotlin/domains/movies/controllers/PersonController.kt
+++ src/main/kotlin/com/kotlin/domains/movies/controllers/PersonController.kt
@@ -1,20 +1,20 @@
-//package com.kotlin.domains.movies.controllers
-//
-//import com.kotlin.domains.movies.dtos.PersonDTO
-//import com.kotlin.domains.movies.services.PersonServiceImplementation
-//import org.springframework.web.bind.annotation.PostMapping
-//import org.springframework.web.bind.annotation.RequestBody
-//import org.springframework.web.bind.annotation.RestController
-//
-//@RestController
-//class PersonController(
-//    private val personServiceImplementation: PersonServiceImplementation
-//) {
-//    @PostMapping("/persons")
-//    fun createPerson(
-//        @RequestBody personDTO: PersonDTO
-//    ): PersonDTO {
-//        return personServiceImplementation.createPerson(personDTO);
-//    }
-//
-//}
+package com.kotlin.domains.movies.controllers
+
+import com.kotlin.domains.movies.dtos.PersonDTO
+import com.kotlin.domains.movies.services.PersonServiceImplementation
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PersonController(
+    private val personServiceImplementation: PersonServiceImplementation
+) {
+    @PostMapping("/persons")
+    fun createPerson(
+        @RequestBody personDTO: PersonDTO
+    ): PersonDTO {
+        return personServiceImplementation.createPerson(personDTO);
+    }
+
+}
